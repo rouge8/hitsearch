94004cd5d0d3e809ad624b1215071ea9f92ed860
Andy Freeland <andy@andyfreeland.net>
2011-05-28 15:09:25 -0500

    database handler

---
+++ database.py
@@ -0,0 +1,20 @@
+# Taken largely from http://flask.pocoo.org/docs/patterns/sqlalchemy/
+# The beautiful thing is that this will all work even if we don't use Flask.
+
+from sqlalchemy import create_engine
+from sqlalchemy.orgm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engin('sqlite:///hitsearch.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    # import all modules here that might define models so that they will be
+    # registered properly on the metadata. Otherwise you will have to import
+    # them first before calling init_db()
+    import hitsearch.models
+    Base.metadata.create_all(bind=engine)
