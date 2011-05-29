7c08163e3745a3dfd1a3e132e820e9d2c0ec5541
Andy Freeland <andy@andyfreeland.net>
2011-05-28 20:59:12 -0500

    --amend

--- search/views.py
+++ search/views.py
@@ -1,5 +1,11 @@
 from search import app
+from search.database import db_session
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.after_request
+def shutdown_session(response):
+    db_session.remove()
+    return response
