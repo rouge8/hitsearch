f55a7e4ef3dea8f11d2b87ad858f6fa1bdd0f58d
Andy Freeland <andy@andyfreeland.net>
2011-06-02 08:47:09 -0500

    set dynamic path for db location. hitsearch.db will be located in the same directory as settings.py regardless of how manage.py is called

--- hitsearch/settings.py
+++ hitsearch/settings.py
@@ -1,4 +1,5 @@
 # Django settings for hitsearch project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hitsearch.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(os.path.dirname(__file__),'hitsearch.db'),
     }
 }
 
