f9b83b56ffe2883b45d1f09739de56b7f79b2c3b
Andy Freeland <andy@andyfreeland.net>
2011-06-03 14:51:06 -0500

    added a few useful modules for the crawler

---
+++ hitsearch/utils.py
@@ -0,0 +1,11 @@
+import string, unicodedata
+
+def sanitize(s):
+    """Normalizes a unicode string to ASCII by removing accents and ignoring
+       all other non-ASCII characters. Then removes punctuation."""
+    # inspired by http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
+    # and http://snippets.dzone.com/posts/show/5499
+
+    normalized = unicodedata.normalize('NFKD', unicode(s)).encode('ASCII', 'ignore')
+    out = normalized.translate(string.maketrans("",""), string.punctuation)
+    return out
