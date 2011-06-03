b7db611ae568cd0292abe8663dd2738629567ea7
Andy Freeland <andy@andyfreeland.net>
2011-06-03 13:28:14 -0500

    removed text attribute from links

--- hitsearch/search/models.py
+++ hitsearch/search/models.py
@@ -13,13 +13,10 @@ class Page(models.Model):
 class Link(models.Model):
     target = models.ForeignKey(Page, related_name='link_target', db_index=True)
     source = models.ForeignKey(Page, related_name='link_source', db_index=True)
-    text = models.TextField(blank=True, null=True)
+    #text = models.TextField(blank=True, null=True)
 
     class Meta:
-        unique_together = ('target', 'source', 'text')
-        # should text also be here?
-        # if so, then it's possible for one source page to have multiple links
-        # to a target page but with different text
+        unique_together = ('target', 'source')
 
     def __unicode__(self):
         output = unicode(self.source) + ' --> ' + unicode(self.target)
