8c11a5b75137840ec7be87b8a5575fa324152868
Andy Freeland <andy@andyfreeland.net>
2011-06-03 11:28:29 -0500

    fixed issue where conrad couldn't run commands with ./hitsearch/manage.py crawl

--- hitsearch/settings.py
+++ hitsearch/settings.py
@@ -113,7 +113,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # search engine!
-    'hitsearch.search',
+    'search',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
