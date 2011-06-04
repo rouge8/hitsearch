cc7e571833343e8f42bbb59badd553d9242d0dae
Daniel Levy <dlouislevy@gmail.com>
2011-06-03 21:18:43 -0500

    added a simple front end to the site

--- hitsearch/search/views.py
+++ hitsearch/search/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from search.models import *
+
+def index(request):
+    return render_to_response('search/index.html', { })
+    
+def results(request):
+    return render_to_response('search/results.html', { })
--- hitsearch/urls.py
+++ hitsearch/urls.py
@@ -15,4 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # search urls
+    url(r'^/?q=(?P<query>.*)', 'search.views.results'),
+    url(r'^', 'search.views.index'),
 )
