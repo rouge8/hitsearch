11e987bdc733457447e1d361f6e6b88cc39c9766
Andy Freeland <andy@andyfreeland.net>
2011-06-04 20:43:39 -0500

    query is correct, albeit slow...

--- hitsearch/query.py
+++ hitsearch/query.py
@@ -6,16 +6,18 @@ def get_results(query, sort_type='authority'):
     terms = query.split()
     terms = [utils.sanitize(term).lower() for term in terms]
     
+    pages = Page.objects.select_related('tag__tag') # this may be super fast?
+
     for term in terms:
-        pages = Page.objects.filter(tag__tag__exact=term)
-    
+        pages = pages.filter(tag__tag=term)
+
     if pages:
         links = dict([(page.url,[link.target.url for link in page.link_source.all()]) for page in pages])
         (authority, hubbiness) = HITS.HITS(links)
 
         for page in pages:
-            print page
-            print authority[page.url]
+            #print page
+            #print authority[page.url]
             page.authority = authority[page.url]
             page.hubbiness = hubbiness[page.url]
 
