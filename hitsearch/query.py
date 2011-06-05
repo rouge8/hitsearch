8eeb9d4e389d0c4379956eea8fda089eb9173674
Andy Freeland <andy@andyfreeland.net>
2011-06-05 11:16:06 -0500

    Drastically reduced the number of database calls in query.py and HOLY SHIT IT IS SO FUCKING FAST NOW. went from 160+ seconds for query 'carleton college' to 2 seconds. :D

--- hitsearch/query.py
+++ hitsearch/query.py
@@ -1,23 +1,36 @@
 from search.models import *
 import HITS
 import utils
+from collections import defaultdict
 
 def get_results(query, sort_type='authority'):
     terms = query.split()
     terms = [utils.sanitize(term).lower() for term in terms]
     
-    pages = Page.objects.select_related('tag__tag') # this may be super fast?
+    pages = Page.objects.select_related() # this may be super fast?
 
     for term in terms:
         pages = pages.filter(tag__tag=term)
 
     if pages:
-        links = dict([(page.url,[link.target.url for link in page.link_source.all()]) for page in pages])
+        links_query = Link.objects.filter(source__in=pages).order_by('source').values_list('source__url', 'target__url')
+
+        links = defaultdict(list)
+        # links_query is a list of tuple links, where links_query[i] = (source_url, target_url)
+        # this beautiful list comprehension works like this:
+        # for link in links_query:
+        #   links[source_url].append(target_url)
+        [links[l[0]].append(l[1]) for l in links_query]
+
+        # this adds any pages that have incoming links but no outgoing links.
+        # maybe we shouldn't use a default dict and should just add them the first time around?
+        for page in pages:
+            if not(page.url in links):
+                links[page.url] = []
+
         (authority, hubbiness) = HITS.HITS(links)
 
         for page in pages:
-            #print page
-            #print authority[page.url]
             page.authority = authority[page.url]
             page.hubbiness = hubbiness[page.url]
 
