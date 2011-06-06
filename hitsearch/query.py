2badd25043cd1109b202c02789af5bee63baf72a
Andy Freeland <andy@andyfreeland.net>
2011-06-05 20:40:51 -0500

    documented query.py

--- hitsearch/query.py
+++ hitsearch/query.py
@@ -1,10 +1,26 @@
+"""
+    query.py
+
+    Queries the database for Page objets matching a query, gets their HITS values,
+    weights by word frequency, and returns a list of all sorted results that
+    matched that query.
+"""
+
 from search.models import *
 import HITS
 import utils
 
-def get_results(query, sort_type, beta):
+def get_results(query, sort_type='authority', beta=1.0):
+    """Queries the database for Page objects matching a query, gets their HITS
+       values, weights them by word frequency, and returns a list of sorted
+       results.
+       sort_type can be either authority or hubbiness
+       beta is the weight of the HITS results vs. the word frequency
+       when beta=1.0 HITS results are alll that matters and word frequency is multiplied by zero
+       when beta=0.0 results are based solely on word frequency."""
+
     terms = query.split()
-    terms = [utils.sanitize(term).lower() for term in terms]
+    terms = [utils.sanitize(term).lower() for term in terms] # strips accents and punctuation
     
     pages = Page.objects.select_related('tag__word_count')
 
@@ -27,19 +43,20 @@ def get_results(query, sort_type, beta):
         # give a boost based on word frequency
         tags_query = Tag.objects.filter(page__in=pages).values_list('page__url', 'word_count', 'tag')
         
+        # creates a dictionary of {page.url: [count of all terms in page, count of all tags in page]
         tags = dict([(page.url, [0, 0]) for page in pages])
         for tag in tags_query:
-            if tag[2] in terms:
-                tags[tag[0]][0] += tag[1]
-            tags[tag[0]][1] += tag[1]
+            if tag[2] in terms: # term
+                tags[tag[0]][0] += tag[1] # add to count of search terms in page
+            tags[tag[0]][1] += tag[1] # add to count of all tags in page
         
-        # assign the pages hubbiness and authority     
+        # assign the pages hubbiness and authority and weights with term frequency   
         for page in pages:
             page.authority = beta * authority[page.url] + (1 - beta) * tags[page.url][0] / float(tags[page.url][1])
             page.hubbiness = beta * hubbiness[page.url] + (1 - beta) * tags[page.url][0] / float(tags[page.url][1])
 
         # sort the pages
-        if sort_type == 'hubbiness': # hubbiness
+        if sort_type == 'hubbiness':
             sorter = lambda page: (page.hubbiness, page.authority)
         else:
             sorter = lambda page: (page.authority, page.hubbiness)
