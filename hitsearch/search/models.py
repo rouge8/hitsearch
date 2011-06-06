8d9f5a6520f719a80bf12d3d4b9373b40046ec5f
Andy Freeland <andy@andyfreeland.net>
2011-06-05 20:53:51 -0500

    documented models.py

--- hitsearch/search/models.py
+++ hitsearch/search/models.py
@@ -1,17 +1,33 @@
+"""
+    models.py
+
+    Implements Page, Link, and Tag models for the search application.
+"""
+
 from django.db import models
 from datetime import datetime
 
 class Page(models.Model):
+    """Page has url, discovery_time, title and optional authority and hubbiness
+       columns. url must be unique."""
+
     url = models.CharField(max_length=500, unique=True, db_index=True)
     discovery_time=(models.DateTimeField(default=datetime.now()))
+    title = models.CharField(max_length=120)
+
+    # authority and hubbiness would be used if we did not perform query-sensitive
+    # HITS
     authority = models.FloatField(blank=True, null=True)
     hubbiness = models.FloatField(blank=True, null=True)
-    title = models.CharField(max_length=120)
 
     def __unicode__(self):
         return self.url
 
 class Link(models.Model):
+    """Link has target and source foreign keys, both of which are Page objects
+       and stored in tables Page.link_target and Page.link_source respectively.
+       (target, source) must be unique."""
+
     target = models.ForeignKey(Page, related_name='link_target', db_index=True)
     source = models.ForeignKey(Page, related_name='link_source', db_index=True)
 
@@ -23,10 +39,15 @@ class Link(models.Model):
         return output
 
 class Tag(models.Model):
+    """Tag has a foreign key page which is a Page object, and fields for tag name,
+       word_count and term_frequency. term_frequency is not currently used in our
+       implementation.
+       (page, tag) must be unique."""
+
     page = models.ForeignKey(Page)
-    word_count = models.IntegerField(blank=True, null=True)
-    term_frequency = models.FloatField(blank=True, null=True)
     tag = models.CharField(max_length=500, db_index=True)
+    word_count = models.IntegerField(blank=True, null=True)
+    term_frequency = models.FloatField(blank=True, null=True) # not actually used
 
     class Meta:
         unique_together = ('page', 'tag')
