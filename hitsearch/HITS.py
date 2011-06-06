461327ed3384d42ab297473798553cee2bf0c3e6
Daniel Levy <dlouislevy@gmail.com>
2011-06-06 13:50:21 -0500

    documented HITS.py

--- hitsearch/HITS.py
+++ hitsearch/HITS.py
@@ -1,7 +1,16 @@
+'''
+HITS.py
+Dan Levy, Conrad Dean, Andy Freeland
+
+An implementation of HITS using the power method and sparse matrices to reduce memory usage. Takes in a dictionary where each key is a page url. The value is a list of links at that page url. See example program in main for how to run.
+'''
+
 def initialize_authority(pages):
+    '''Initializes the authority vector, a dictionary where the keys are the urls and the values are all 1'''
     return dict(zip(pages.keys(), [1]*len(pages)))
 
 def clean_pages(pages):
+    '''Removes links to pages outside the set of pages we are currently running HITS on'''
     for page in pages:
         outside_links = []
         for i in range(len(pages[page])):
@@ -12,6 +21,7 @@ def clean_pages(pages):
     return pages
                 
 def initialize_L_matrices(pages):
+    '''Initializes L, which is just the pages dictionary, and then computes the transpose of L. Matrices are pretty compact since we only store non zero cells.'''
     L_matrix = pages
     Lt_matrix = {}
     for page in pages:
@@ -22,6 +32,7 @@ def initialize_L_matrices(pages):
     return L_matrix, Lt_matrix
 
 def multiply_matrix_vector(matrix, vector):
+    '''Multiplies a matrix and a vector'''
     result_matrix = {}
     for row in matrix:
         result_matrix[row] = 0
@@ -30,6 +41,7 @@ def multiply_matrix_vector(matrix, vector):
     return result_matrix        
             
 def normalize(vector):
+    '''Takes a vector and divides all components by the component with the max value. This means that the largest value in the vector will be 1.'''
     max = 0
     for component in vector:
         if vector[component] > max:
@@ -41,6 +53,7 @@ def normalize(vector):
     return vector
         
 def vector_difference(vector1, vector2):
+    '''Returns the sum of all of the differences between components in vector1 and vector2.'''
     if not (vector1 and vector2): return float("inf")
     total = 0
     for component in vector1:
@@ -48,6 +61,7 @@ def vector_difference(vector1, vector2):
     return total        
 
 def HITS(pages):
+    '''Runs HITS'''
     pages = clean_pages(pages)
     authority_old = None
     authority = initialize_authority(pages)
@@ -59,6 +73,7 @@ def HITS(pages):
     return authority, hubbiness
 
 def main():
+    '''A simple example of HITS. Page a has links to b and c. Page b links to f. Page c links to b and e. Etc.'''
     pages = {"a":["b", "c"], "b":["f"], "c":["b", "e"], "d":["b"], "e":["c"]}
     (authority, hubbiness) = HITS(pages)
     print "Authority: " + str(authority)
