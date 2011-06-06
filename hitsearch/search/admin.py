e6f1097b1389acd45148e4504b00babb62f5643e
Conrad <cheesemold@gmail.com>
2011-06-06 01:51:27 -0500

    got some filtering and search stuff working-- but doesn't really work for tags/links

--- hitsearch/search/admin.py
+++ hitsearch/search/admin.py
@@ -3,16 +3,20 @@ from django.contrib import admin
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ("title" , "url", "discovery_time")
+    list_filter = ["discovery_time"]
+    search_fields = ["title"]
 
 admin.site.register(Page,PageAdmin)
 
 class LinkAdmin(admin.ModelAdmin):
     list_display = ( "source", "target")
+    #search_fields = ["source", "target"]  # Cant get this to work
 
 admin.site.register(Link,LinkAdmin)
 
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ( "tag", "page", "word_count", "term_frequency")
+    #search_fields = ["tag", "page"]  # Cant get this to work
 
 admin.site.register(Tag,TagAdmin)
