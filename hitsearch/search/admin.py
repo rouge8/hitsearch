11bbfb00dddeef5817750551f5d78b37fdc09c88
Conrad <cheesemold@gmail.com>
2011-06-06 01:56:46 -0500

    some filters.  eh why not

--- hitsearch/search/admin.py
+++ hitsearch/search/admin.py
@@ -10,6 +10,7 @@ admin.site.register(Page,PageAdmin)
 
 class LinkAdmin(admin.ModelAdmin):
     list_display = ( "source", "target")
+    list_filter = ["source"]
     #search_fields = ["source", "target"]  # Cant get this to work
 
 admin.site.register(Link,LinkAdmin)
@@ -17,6 +18,7 @@ admin.site.register(Link,LinkAdmin)
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ( "tag", "page", "word_count", "term_frequency")
+    list_filter = ["tag","page"]
     #search_fields = ["tag", "page"]  # Cant get this to work
 
 admin.site.register(Tag,TagAdmin)
